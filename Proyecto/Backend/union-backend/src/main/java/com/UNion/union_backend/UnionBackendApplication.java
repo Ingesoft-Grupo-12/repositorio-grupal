0032e2473fe7aeb58e5938d8002ee4103847a87d
Miguel Martinez <miguel.martinez@nuvu.cc>
2025-01-31 19:46:40 -0500

    iniciacion proyecto y linter

---
+++ Proyecto/Backend/union-backend/src/main/java/com/UNion/union_backend/UnionBackendApplication.java
@@ -0,0 +1,17 @@
+package com.UNion.union_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal que inicia la aplicación Spring Boot.
+ */
+
+@SpringBootApplication
+public class UnionBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnionBackendApplication.class, args);
+  }
+
+}
