545baecbdf2ebeb59603e67df229f223c8ac3290
Miguel Martinez <miguel.martinez@nuvu.cc>
2025-02-04 19:41:02 -0500

    test

---
+++ Proyecto/Backend/union-backend/src/test/java/com/union/unionbackend/services/impl/UserServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.union.union_backend.services.Impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.union.unionbackend.models.User;
+import com.union.unionbackend.repositories.UserRepository;
+import com.union.unionbackend.services.impl.UserServiceImpl;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+
+class UserServiceImplTest {
+
+  @Mock
+  private UserRepository userRepository;
+
+  @InjectMocks
+  private UserServiceImpl userService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void getAllUsers() {
+    //arrrange
+    User user1 = new User(1L, "test_user", "teacher@gmail.com", "teacher", "image_example");
+    List<User> users = List.of(user1);
+
+    //act
+    when(userRepository.findAll()).thenReturn((users));
+    List<User> usersTest = userService.getAllUsers();
+
+    //assert
+    assertEquals(usersTest, users);
+  }
+
+  @Test
+  void getUsersByRole(){}
+}
