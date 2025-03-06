8ebfb72080e60f83675b9c138e034cd84538fd34
EFP210 <edwinpinilla125@gmail.com>
2025-03-06 11:11:29 -0500

    userDTO fix

--- Proyecto/Backend/union-backend/src/main/java/com/union/unionbackend/mapper/UserMapper.java
+++ Proyecto/Backend/union-backend/src/main/java/com/union/unionbackend/mapper/UserMapper.java
@@ -4,16 +4,19 @@ import com.union.unionbackend.dtos.UserDto;
 import com.union.unionbackend.models.User;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
-
   UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+  @Mapping(source = "userimage", target = "userimage")
   UserDto userToDto(User user);
 
+  @Mapping(source = "userimage", target = "userimage")
   User dtoToUser(UserDto userDto);
 
   List<UserDto> usersToUserDtos(List<User> users);
 }
+
